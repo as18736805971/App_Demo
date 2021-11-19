baa4435f3bab98525ea4d418a199a8f66877b3ea
赵文闯 <as18736805971@gmail.com>
2021-11-19 16:32:28 +0800

    首页及图标 列表组件

---
+++ components/components/list.vue
@@ -0,0 +1,124 @@
+<template>
+	<view class="listmenu">
+		<view class="list-container">
+			<view class="bg">
+				<template v-for="(item, index) in list">
+					<view v-if="item.hidden" :key="index" class="menu-item" @click="handleJump(item, index)">
+						<p class="lefttit">
+							<image v-if="item.show_icon" class="menu-item-icon" :src="item.icon"></image>
+							<span class="tit">
+								{{ item.content }}
+							</span>
+						</p>
+						<p v-if="item.show_more" class="righttit">
+							<span class="a_title">
+								{{ item.more_text }}
+							</span>
+							<image v-if="item.right_icon" class="menu-item-righttit-icon" :src="item.right_icon">
+							</image>
+						</p>
+					</view>
+				</template>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		props: {
+			list: {
+				type: Array,
+				default: () => []
+			}
+		},
+		data() {
+			return {}
+		},
+		methods: {
+			handleJump(item, index) {
+				this.$emit('handleListJump', {
+					item,
+					index
+				})
+			}
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	.listmenu {
+		padding: 10rpx 20rpx;
+
+		.list-container {
+			padding-left: 24rpx;
+			padding-right: 24rpx;
+			background-color: #FFFFFF;
+			border-radius: 12rpx;
+		}
+
+		.bg {
+			width: 100%;
+			display: flex;
+			position: relative;
+			box-sizing: border-box;
+			flex-direction: column;
+
+			.menu-item {
+				width: 100%;
+				display: flex;
+				position: relative;
+				box-sizing: border-box;
+				height: 96rpx;
+				border-top: 0 !important;
+				border-left: 0 !important;
+				border-right: 0 !important;
+				border-bottom: 1px solid #eee;
+			}
+
+			.menu-item:last-child {
+				border-bottom: 0 !important;
+			}
+
+			.lefttit {
+				display: flex;
+				font-size: 28rpx;
+				justify-content: center;
+				align-items: center;
+
+				span {
+					//margin: auto;
+					margin-left: 8rpx;
+					line-height: 36rpx;
+				}
+
+				.menu-item-icon {
+					width: 40rpx;
+					height: 40rpx;
+				}
+			}
+
+			.righttit {
+				position: absolute;
+				cursor: pointer;
+				right: 0;
+				display: flex;
+				top: 0;
+				bottom: 0;
+				margin: auto;
+				justify-content: center;
+				align-items: center;
+				font-size: 24rpx;
+				font-style: normal;
+				font-weight: normal;
+				color: #8a8a8a;
+
+				.menu-item-righttit-icon {
+					width: 20rpx;
+					height: 20rpx;
+					margin-left: 5rpx;
+				}
+			}
+		}
+	}
+</style>
--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,20 +1,38 @@
 <template>
 	<view class="page-index">
+		<!-- 轮播图组件 -->
 		<Shuffling :img-list="imgList" @selected="selectedBanner"></Shuffling>
+		<!-- 轮播图组件 -->
+
+		<!-- 列表导航 -->
+		<view class="navigation margin">
+			<view class="item" v-for="(item, index) in navigation" :key="index" @click="handleNavigation(item, index)">
+				<image class="icon" mode="aspectFit" :src="item.icon"></image>
+				<view class="title">{{item.title}}</view>
+			</view>
+		</view>
+		<!-- 列表导航 -->
+
+		<!-- 列表组件 -->
+		<diy-list :list="list" @handleListJump="handleList"></diy-list>
+		<!-- 列表组件 -->
 	</view>
 </template>
 
 <script>
-	import Shuffling from '@/components/components/Shuffling.vue'
+	import Shuffling from '@/components/components/Shuffling'
+	import diyList from '@/components/components/list'
+
 	export default {
 		components: {
 			Shuffling,
+			diyList,
 		},
 		data() {
 			return {
 				imgList: [{
 					url: 'https://img9.51tietu.net/pic/2019-091200/vgkpidei2tjvgkpidei2tj.jpg',
-					id: 1
+					id: 1,
 				}, {
 					url: 'https://img9.51tietu.net/pic/2019-091200/euzekmi5m23euzekmi5m23.jpg',
 					id: 2
@@ -24,21 +42,157 @@
 				}, {
 					url: 'https://img9.51tietu.net/pic/2019-091200/ff1vqwm3q33ff1vqwm3q33.jpg',
 					id: 4
-				}, ]
+				}, ],
+				navigation: [{
+					id: 1,
+					title: '地图',
+					icon: require('@/static/appicon/address.png'),
+				},{
+          id: 1,
+          title: '文章列表',
+          icon: require('@/static/appicon/app.png'),
+        },{
+          id: 1,
+          title: '文章列表',
+          icon: require('@/static/appicon/article.png'),
+        },{
+          id: 1,
+          title: '文章列表',
+          icon: require('@/static/appicon/cate.png'),
+        },{
+          id: 1,
+          title: '文章列表',
+          icon: require('@/static/appicon/close.png'),
+        },{
+          id: 1,
+          title: '文章列表',
+          icon: require('@/static/appicon/code.png'),
+        },{
+          id: 1,
+          title: '文章列表',
+          icon: require('@/static/appicon/coupon.png'),
+        },{
+          id: 1,
+          title: '文章列表',
+          icon: require('@/static/appicon/data.png'),
+        },{
+          id: 1,
+          title: '文章列表',
+          icon: require('@/static/appicon/email.png'),
+        },{
+          id: 1,
+          title: '文章列表',
+          icon: require('@/static/appicon/file.png'),
+        },],
+				list: [{
+					id: 1,
+					hidden: true,
+					show_more: true,
+					more_text: '更多',
+					right_icon: require('@/static/icon/right_arrow.png'),
+					show_icon: true,
+					icon: require('@/static/appicon/data.png'),
+					content: '列表标题',
+				}, {
+					id: 2,
+					hidden: true,
+					show_more: true,
+					more_text: '更多',
+					right_icon: require('@/static/icon/right_arrow.png'),
+					show_icon: true,
+					icon: require('@/static/appicon/goods.png'),
+					content: '商品列表',
+				}, {
+					id: 3,
+					hidden: true,
+					show_more: true,
+					more_text: '更多',
+					right_icon: require('@/static/icon/right_arrow.png'),
+					show_icon: true,
+					icon: require('@/static/appicon/image.png'),
+					content: '图片列表',
+				}, {
+					id: 4,
+					hidden: true,
+					show_more: false,
+					more_text: '更多',
+					right_icon: require('@/static/icon/right_arrow.png'),
+					show_icon: true,
+					icon: require('@/static/appicon/about.png'),
+					content: '关于我们',
+				}]
 			}
 		},
 		onLoad() {},
 		methods: {
+			// 点击轮播图图片
 			selectedBanner(item, index) {
 				console.log('🥒', item, index)
-			}
+			},
+			// 点击导航列表
+			handleNavigation(item, index) {
+				console.log('🥒', item, index)
+			},
+			// 点击列表
+			handleList(data) {
+				console.log('🥒', data)
+			},
 		}
 	}
 </script>
 
 <style lang="scss">
+	page {
+		background-color: #f4f6f9;
+	}
+
 	.page-index {
 		width: 100%;
 		min-height: 100vh;
+
+		.margin {
+			margin-bottom: 10rpx !important;
+		}
+
+		/* 列表导航  */
+		.navigation {
+			width: 90%;
+			height: auto;
+			margin: 0 auto;
+			background-color: #FFFFFF;
+			// box-shadow: 0 0 10rpx rgba(0, 0, 0, 0.4);
+			border-radius: 12rpx;
+			padding: 20rpx 20rpx 0;
+			display: flex;
+			flex-wrap: wrap;
+			justify-content: space-between;
+
+			.item {
+				width: 130rpx;
+				height: 130rpx;
+				margin-bottom: 20rpx;
+				border-radius: 10rpx;
+				display: flex;
+				align-items: center;
+				justify-content: center;
+				flex-wrap: wrap;
+
+				.icon {
+					width: 90rpx;
+					height: 90rpx;
+					border-radius: 10rpx;
+				}
+
+				.title {
+					font-size: 24rpx;
+					text-align: center;
+				}
+			}
+		}
+
+		/* 列表导航  */
+
+		/* 列表组件 */
+		/* 列表组件 */
 	}
 </style>
